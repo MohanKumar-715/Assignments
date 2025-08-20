85f51766868cef50c4274e97aee9e470d0c8c8f3
MohanKumar-715 <mohankumarbh007@gmail.com>
2025-08-20 21:42:49 +0530

    Add files via upload

---
+++ Fact.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial does not exist for negative numbers.")
+else:
+    print(f"The factorial of {num} is {factorial(num)}")
