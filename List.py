16291ae8eeff7c787aa316a0263ebe7bb74e5af2
Mohan Kumar B.H <mohankumarbh007@gmail.com>
2025-08-30 18:05:16 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,12 @@
+
+numbers = list(range(1, 11))
+
+
+first_five = numbers[:5]
+
+
+reversed_list = first_five[::-1]
+
+# Step 4: Print both lists
+print("Extracted list:", first_five)
+print("Reversed list:", reversed_list)
